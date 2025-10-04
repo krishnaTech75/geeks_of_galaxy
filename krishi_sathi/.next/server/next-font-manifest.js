288bee0c4ee8fb653f78ee06cf2be4bcd188be29
krishnaTech75 <krishna.tech.guru.30oct@gmail.com>
2025-10-04 22:43:28 +0530

    Upload krishi_sathi/.next/server/next-font-manifest.js

---
+++ krishi_sathi/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/workspaces/secura/Geeks_of_galaxy/krishi_sathi/app/layout\":[\"static/media/5b01f339abf2f1a5.p.woff2\",\"static/media/028c0d39d2e8f589-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
